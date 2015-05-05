3f36fa0fa23a937124e1cefd7d27f96506f7697d
Michael Hyland <michaelhyland.gti@gmail.com>
2015-05-05 22:39:20 +0100

    Create AdminCommands.java

---
+++ AdminCommands.java
@@ -0,0 +1,18 @@
+
+package energyreport;
+
+/**
+ *
+ * @author Kobevader
+ */
+public interface AdminCommands { //Commands requiring administrative permission
+    
+    
+    
+    
+    
+     public void energyReport(); //Generates a comprehensive energy report
+     
+     
+     
+}
